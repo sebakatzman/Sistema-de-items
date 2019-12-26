35131a263ba20790137b53619818b60e2d561e55
Sebakatzman <k.sebastianariel@gmail.com>
2019-12-26 20:27:46 -0300

    Primer commit

---
+++ SistemaDeItems/src/Estado.java
@@ -0,0 +1,20 @@
+
+public class Estado {
+    private String estado;
+    
+    public Estado() {
+        estado = "";
+    }
+    
+    public Estado(String nombre) {
+        estado = nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
